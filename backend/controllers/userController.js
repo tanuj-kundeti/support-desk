24cce56c6649e41c0142bf103f8c57d6074b5e28
tkundeti <tkundeti@iu.edu>
2024-02-13 18:31:43 -0500

    modified few files

---
+++ backend/controllers/userController.js
@@ -0,0 +1,54 @@
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/userModel");
+
+// @desc   Register a new user
+// @route  /api/users
+// @ access Public
+const registerUser = asyncHandler(async (req, res) => {
+  //console.log(req.body)
+  const { name, email, password } = req.body;
+
+  //validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  // Find if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({ name, email, password: hashedPassword });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
+  // res.send("Register Route");
+});
+
+// @desc   Login a user
+// @route  /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+  res.send("Login Route");
+});
+
+module.exports = { registerUser, loginUser };
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 5000;
+
+//Connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(errorHandler);
+
+app.get("/", (req, res) => {
+  res.status(201).json({ hi: "Hi my name is Tanuj" });
+});
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
