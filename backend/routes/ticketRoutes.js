f0390f337345cf3d4690316b78befe093cad83c2
tkundeti <tkundeti@iu.edu>
2024-02-22 17:48:47 -0500

    updated ticket routes in backend

---
+++ backend/routes/ticketRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getTickets,
+  createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+} = require("../controllers/ticketController");
+
+const { protect } = require("../middleware/authMiddleware");
+
+router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .put(protect, updateTicket)
+  .delete(protect, deleteTicket);
+
+module.exports = router;
